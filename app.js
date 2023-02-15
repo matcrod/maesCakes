e21c43d1cddc5739c9cefdbc3544f76e28a1f1dc
M. C. Rodriguez <matchrrod@gmail.com>
2023-02-15 09:28:46 -0600

    adds background image to main, modifies functionality

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const burger = document.querySelector('[data-burger]')
+const navLinks = document.querySelector('.nav-links')
+const bar1 = document.querySelector('[data-bar1]')
+const bar3 = document.querySelector('[data-bar3]')
+
+burger.addEventListener('click', () => {
+	navLinks.classList.toggle('nav-active')
+    bar1.classList.toggle('bars')
+    bar3.classList.toggle('bars')
+})
